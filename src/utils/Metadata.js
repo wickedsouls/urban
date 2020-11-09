6fe976edc22742b806620c842036efbe1d84844d
wickedsouls <karolis.kerpys@gmail.com>
2020-11-09 03:46:35 +0200

    Delete index.js

--- src/utils/Metadata.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var Metadata;
-(function (Metadata) {
-    Metadata["method"] = "method";
-    Metadata["path"] = "math";
-    Metadata["validator"] = "validator";
-    Metadata["use"] = "use";
-})(Metadata = exports.Metadata || (exports.Metadata = {}));
--- src/utils/Methods.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var Methods;
-(function (Methods) {
-    Methods["get"] = "get";
-    Methods["post"] = "post";
-    Methods["delete"] = "delete";
-    Methods["put"] = "put";
-})(Methods = exports.Methods || (exports.Methods = {}));
